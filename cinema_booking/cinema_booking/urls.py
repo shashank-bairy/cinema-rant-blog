a0c1a3cfda2031ec634161c6f71e4a0ff748d3f2
Shashank Bairy R <shashank.bairy04@gmail.com>
2019-06-11 00:26:00 +0530

    Restructuring project directory

--- cinema_booking/cinema_booking/urls.py
+++ cinema_booking/cinema_booking/urls.py
@@ -1,4 +1,4 @@
-"""blog URL Configuration
+"""cinema_booking URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
