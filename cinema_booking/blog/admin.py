0f7a580ce4bf81412b001e8b7ba45bd31642fdf3
Shashank Bairy R <shashank.bairy04@gmail.com>
2019-06-13 23:09:30 +0530

    Build check

--- cinema_booking/blog/admin.py
+++ cinema_booking/blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Category, Post, Comment
 
 # Register Category model
-admin.site.register(Category)
+admin.site.register(Category
 # Register Post model
 admin.site.register(Post)
 # Register Comment model
