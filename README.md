0c3dfe2f98bb5bc9b1bf639a272684595924f152
Shashank Bairy R <shashank.bairy04@gmail.com>
2019-08-03 16:00:21 +0530

    Added documentation

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
-# cinema-booking-website
-
-[![Build Status](https://shashankbairy.visualstudio.com/cinema-booking-website/_apis/build/status/cinema-booking-website?branchName=master)](https://shashankbairy.visualstudio.com/cinema-booking-website/_build/latest?definitionId=2&branchName=master)
+# CinemaRant
 
 A website for booking cinema tickets which seeks to improve the cinema occupancy rates across the country.
+
+## Installation
+
+Fork the repository to your github profile and clone the repository to your local system.
+
+Once you have coloned the repository. Perform
+
+```bash
+pip install -r requirements.txt
+```
+
+This installs all the necessary package dependencies if not already installed.
+
+Next run the migrations
+
+```bash
+python manage.py migrate
+```
+
+**Make sure that you have connected the required database**
+
+- sqlite3 in Development mode
+- Postgres in Production mode
+  ( [**Instructions to setup Postgres**](https://www.digitalocean.com/community/tutorials/how-to-use-postgresql-with-your-django-application-on-ubuntu-14-04) )
+
+Create admin user using below command and following the instructions after that
+
+```bash
+python manage.py createsuperuser
+```
+
+## Usage
+
+To run the app
+
+```bash
+python manage.py runserver
+```
--- cinema_booking/blog/views.py
+++ cinema_booking/blog/views.py
@@ -2,34 +2,38 @@ from django.shortcuts import render
 from .models import Category, Post
 from django.views.generic import (ListView, DetailView)
 
-
+# View to display results on searching for posts
 class PostSearchView(ListView):
     model = Post
     template_name = 'blog/post_search.html'
     context_object_name = 'posts'
 
     def get_queryset(self):
+        # getting the search string from search request
         post_title = self.request.GET.get('post_title')
+        # get posts that contain the search string
         queryset = Post.objects.filter(title__icontains=post_title)
         return queryset
 
-
+# Home page view -> listing the posts posted so far
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
 
-
+# List the posts according to the categories
 class PostCategoryListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
 
     def get_queryset(self):
+        # getting slug of category from URL
         slug = self.kwargs.get('slug')
+        # getting posts that are of same category
         queryset = Post.objects.filter(category__slug__iexact=slug)
         return queryset
 
-
+# Display the post and its content
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
